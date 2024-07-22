4398aeae5a7ec2ccf696723ffdde7b63e52f0495
Klaus Mezger <klaus2@bluewin.ch>
2024-07-22 16:52:00 +0200

    removed unused code

--- popSearchCharger.py
+++ popSearchCharger.py
@@ -6,12 +6,8 @@ import socket
 from charger import search_charger
 x=sg.version
 
-manualSettings = {'cancelled': True, 'currentSet': 10, 'chargeLimit': 90, '3_phases': False}
-
 #b64Logo=b"iVBORw0KGgoAAAANSUhEUgAAAA4AAAAQCAYAAAAmlE46AAAACXBIWXMAAA7DAAAOwwHHb6hkAAAAGXRFWHRTb2Z0d2FyZQB3d3cuaW5rc2NhcGUub3Jnm+48GgAAAMNJREFUKJGVkr0NwjAUhO9ZGYCCGRLZmYIRmAYoyQAUiAkYgIaCMhUNveMmI1BQUCBEdDT8JWDjXGf7fXfvJINab2lMjp5SEBmB3NOYJdN0GAsKjTm/TuQJSi0ArMTaazixZSMDkAXIA7Uexyd2RZYQmUpV2XDil62/fzixnd7qHw++VYOch1f1SeSSRA93Vv0PkjeIrNE0hTh3fF6HQbKEUhOxtuo++cAawEyc2/k822CPL5c8gJ89gmKeb5hlWdTwh+56AWOaUxHtDQAAAABJRU5ErkJggg=="
 b64Logo=b'iVBORw0KGgoAAAANSUhEUgAAABoAAAAeCAYAAAAy2w7YAAAACXBIWXMAAA7DAAAOwwHHb6hkAAAAGXRFWHRTb2Z0d2FyZQB3d3cuaW5rc2NhcGUub3Jnm+48GgAAAqlJREFUSIm11U9IFGEYBvDnnf3njFmrXoog8iBBRyGIKEiQYHcriQjqUiB0qKBLsIZetEtQKCUI/aGTVIiQQuWGFQhZRJFE4aGDFJpIy7azq7s7uzsz39tBGFcdd3fW9rnNzPd8v/mYb2agBpXny4Ft+1HlSAC3mpJ4r4bkgaVTdY1VhABm9oDRYeaMr8mQcpnPwFUVqCD1gvlWIqN8SoRq26oJrYR5H7MYS4TkkcSxmqbqQZaHANzSl3hAvs0nUVc1CAAY7CXCpaQpT6sBpYN7SncqgiyQsQvEA4nP8mQyIB+sGrQqokVIeK0G5aH4CXlPuTVSg3IKADwtbS75yl2f3SCt/2JWn/kgNpRBGRDuqOlMf9MkssUgt1WqqYW0cy/ZjvIptucZrIDRVa/IF+JB9NaPa08J4KJQYdLXWjVTjVoFVv/YlldB7CbggRpQzseZwg2v0t/KgszYAovYQtHJ7ULEh0E8pQaVYbchdddNpKJFIc+R026kEgwALEzk3z42HHgSwOcMjxlUg3JfjLXB5ghy1mbwHmp3Kd1Pata32NCRbPenHS6uIDQLiXtsV6RPjRqcy6wcmGblxkqYTcraQtrDznwlz2hNCEkw+mKcGWyOIGcLbTECoGG3XsZm8AU63CK9ZB3rU6OGiM6VXCGB3glQuGE8/X39NXvo7HXvmlv8NSNEdG7Th0XAbwHc8I9nSr+w5uIs51/c0+0Giei8bdnJJ8ja3o5CYDDGhIGuxgltvpyK881AmJYEwjsi2kcntbJ/E0RYBNNV/wHtqFMEKGNFBMoL5kf+Wq2XRpBCxClRBkSECHQRbpjI/qxs+lIQ0Q8Cdfpfpt9sFdgMUiWim9uVzH0awZY/chsgItIZPOTyunu3jy7//Z+AFTUkP0se9zVXZfKC/AOggCeZWmV4NwAAAABJRU5ErkJggg=='
-#b64Logo = b'iVBORw0KGgoAAAANSUhEUgAAAAwAAAAOCAYAAAAbvf3sAAAACXBIWXMAAA7DAAAOwwHHb6hkAAAAGXRFWHRTb2Z0d2FyZQB3d3cuaW5rc2NhcGUub3Jnm+48GgAAAO1JREFUKJGNz69Lg1EUxvHveZ0aBINZ28p7GYLJ7J/gn7B3KwprgiCCQ7AoBrHOH2CxmwSLRTCIZSrCMNiWdeCY3sd0BcfuuE8853zO4aAiv1K9kpOYDGwF7+9UdQdqlGcTAACTGGv0ptqq5etq/tWjIGQO2T7v7lZVt5wCQpbIuFHNXai+OJ8CQBhiFf/9oMJtqVGeBjAV7jOK/m94A3biF0bdxL5KCYM94IiZwaEdd/pxYAh0iU1sW6vdDeUYeMSzYWcv98ONYdDF2GPh+dya+FGbAhggWtjPrp28fox7qITpGrJNO33qjBsM+QXm+0kImC1DaAAAAABJRU5ErkJggg=='
-# todo: function parameter replace with manualSettings ??
 
 def popSearchCharger():
     """ Search based on local wifi ip of the host. """
@@ -32,7 +28,7 @@ def popSearchCharger():
 
     popWin = sg.Window('Search Charger IP', layout_popC, icon=b64Logo)
     while True:
-        ev2, val2 = popWin.read(100)
+        ev2, val2 = popWin.read()
         if ev2 == sg.WIN_CLOSED or ev2 == 'EXIT':
            #    print(manualSettings)
             break
